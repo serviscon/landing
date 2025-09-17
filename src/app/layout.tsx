be999e98d34156e2dab7b49da524171995fe22e1
OAB Seccional do Maranhão <informatica@oabma.org.br>
2025-09-17 15:32:07 -0300

    Google Tag Manager Adicionado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const geist = Geist({
   subsets: ['latin'],
@@ -21,7 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased ${geist.className}`}>{children}</body>
+      <body className={`antialiased ${geist.className}`}>
+        {children}
+
+        {/* Google Tag Manager */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11002985895" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11002985895');
+          `}
+        </Script>
+      </body>
     </html>
   )
 }
